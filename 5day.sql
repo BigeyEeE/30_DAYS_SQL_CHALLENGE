7abf5832dd3eb63702b9aa4e59aea03975e190fd
Tushar SIngh <149139652+BigeyEeE@users.noreply.github.com>
2024-04-20 23:01:55 +0530

    5day.sql

---
+++ 5day.sql
@@ -0,0 +1,54 @@
+-- Day 05/30 days SQL challenge
+
+-- SCHEMAS
+
+
+-- Create transactions table
+CREATE TABLE transactions (
+    user_id INTEGER,
+    spend DECIMAL(10, 2),
+    transaction_date TIMESTAMP
+);
+
+-- Insert data into transactions table
+INSERT INTO transactions (user_id, spend, transaction_date) VALUES
+(111, 100.50, '2022-01-08 12:00:00'),
+(111, 55.00, '2022-01-10 12:00:00'),
+(121, 36.00, '2022-01-18 12:00:00'),
+(145, 24.99, '2022-01-26 12:00:00'),
+(111, 89.60, '2022-02-05 12:00:00');
+
+/*
+-- UBER DATA ANALYST INTERVIEW QUESTION
+Question:
+Write a SQL query to obtain the third transaction of every user. 
+Output the user id, spend, and transaction date.
+
+*/
+
+
+
+
+
+-- ----------------------------------------------
+-- My solution
+-- ----------------------------------------------
+
+
+
+
+
+
+SELECT 
+  user_id,
+  spend,
+  transaction_date
+FROM 
+    (
+      SELECT 
+        user_id,
+        spend,
+        transaction_date,
+        ROW_NUMBER() OVER(PARTITION BY user_id ORDER BY transaction_date) as rn
+    FROM transactions) x1
+WHERE rn = 3
